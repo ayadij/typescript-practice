6b8779e08e602cdd1e355feefafd9ffb2f5aa771
ayadij <ayadijkwel@yahoo.com>
2018-06-27 11:09:22 -0600

    create a class

---
+++ class-dfn.ts
@@ -0,0 +1,14 @@
+class Tree {
+    branch: string
+    constuctor(branch){
+        this.branch = branch;
+    }
+}
+
+
+// define in one line
+class Person {
+    constructor(public name:string){
+        this.name = name;
+    }
+}
