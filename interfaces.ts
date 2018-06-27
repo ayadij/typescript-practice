2ff06a21f8159c53afbee770c38835c914f28f90
ayadij <ayadijkwel@yahoo.com>
2018-06-27 11:02:37 -0600

    interfaces and class implements interface

---
+++ interfaces.ts
@@ -0,0 +1,25 @@
+interface MotorcycleInterface{
+    make: string;
+    model?: string;
+    cc: number;
+    describeBike(make:string, cc:number):void
+}
+
+const motorcycle: MotorcycleInterface = {
+    make: "ducati",
+    model?: "monster",
+    cc: 750,
+    describeBike(make:string,cc:number){
+        console.log(`this ${this.make} is ${cc}cc`);
+    }
+}
+
+// syntax to apply an interface to the Class
+class MotorcycleClass implements MotorcycleInterface {
+    make: string;
+    model?: string; //question mark makes it optional
+    cc: number;
+    describeBike(make:string,cc:number):void {
+
+    }
+}
